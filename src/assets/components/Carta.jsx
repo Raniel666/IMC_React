3e3bad78a93f59b3b5f73a0cafd6bfab62421a0a
Raniel666 <lcaos666@gmail.com>
2023-09-18 07:29:56 -0400

    Agregue el Spinner

--- src/assets/components/Carta.jsx
+++ src/assets/components/Carta.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import '../../assets/Carta.css'
+import Spinner from './Spinner';
 
 
 export const Carta = ({IMC,setgender,gender}) => {
@@ -7,7 +8,7 @@ export const Carta = ({IMC,setgender,gender}) => {
     const calculo = (parseFloat(weight) / (parseInt(height) / 100) ** 2).toFixed(2);
     const [resultIMC, setresultIMC] = useState(calculo)
     const [Estado, setEstado] = useState('')
-    const [img, setimg] = useState('')
+    const [img, setimg] = useState(null)
 
 
 
@@ -69,36 +70,36 @@ export const Carta = ({IMC,setgender,gender}) => {
 
 
   return (
-    <div className="card">
-  <div className="top-section">
-    <div className="border"></div>
-    <div className="icons">
-      <div className="logo">
-       <button onClick={reset}>Regresar</button>
+    img?(    <div className="card">
+    <div className="top-section">
+      <div className="border"></div>
+      <div className="icons">
+        <div className="logo">
+         <button onClick={reset}>Regresar</button>
+        </div>
+      
       </div>
+      
     
+      <img src={img} className='img-characther' alt="" />
     </div>
-    
-  
-    <img src={img} className='img-characther' alt="" />
-  </div>
-  <div className="bottom-section">
-    <span className="title">Hola! tienes {Estado}</span>
-    <div className="row row1">
-      <div className="item">
-        <span className="big-text">Peso</span>
-        <span className="regular-text">{IMC.weight} kg</span>
-      </div>
-      <div className="item">
-        <span className="big-text">IMC </span>
-        <span className="regular-text">{resultIMC} kg/cm</span>
-      </div>
-      <div className="item">
-        <span className="big-text">Altura</span>
-        <span className="regular-text">{IMC.height} cm</span>
+    <div className="bottom-section">
+      <span className="title">Hola! tienes {Estado}</span>
+      <div className="row row1">
+        <div className="item">
+          <span className="big-text">Peso</span>
+          <span className="regular-text">{IMC.weight} kg</span>
+        </div>
+        <div className="item">
+          <span className="big-text">IMC </span>
+          <span className="regular-text">{resultIMC} kg/cm</span>
+        </div>
+        <div className="item">
+          <span className="big-text">Altura</span>
+          <span className="regular-text">{IMC.height} cm</span>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+  </div>):<Spinner></Spinner>
   )
 }
---
+++ src/assets/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../assets/Spinner.css'
+const Spinner = () => {
+  return (
+<div class="honeycomb">
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+  )
+}
+
+export default Spinner
