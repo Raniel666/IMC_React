9f231b747ab1ef7b3f16f2c9913f65450b04a404
Raniel666 <lcaos666@gmail.com>
2023-09-17 22:26:53 -0400

    k

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
   return (
       gender=="" ?(           
       <section className='container_button'>
-        <ButtonGenero img="../public/img/btn_boy.png"  setgender={setgender} gender={gender} >Masculino</ButtonGenero>
-        <ButtonGenero img="../public/img/btn_women.png" setgender={setgender} gender={gender} >Femenino</ButtonGenero>   
+        <ButtonGenero img="./img/btn_boy.png"  setgender={setgender} gender={gender} >Masculino</ButtonGenero>
+        <ButtonGenero img="./img/btn_women.png" setgender={setgender} gender={gender} >Femenino</ButtonGenero>   
       </section>)
 
 :
