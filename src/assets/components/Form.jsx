d9944d6bed7fbc058fa88ef160f3fee896892737
Raniel666 <lcaos666@gmail.com>
2023-09-21 17:06:14 -0400

    Agregando el Effect

--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -5,6 +5,8 @@ export const Form = ({gender,setgender}) => {
 
 const [result, setresult] = useState(0)
 const [succesful, setsuccesful] = useState(false)
+const  [error,setError] = useState(false)
+
 let calculo=0
 const [IMC, setIMC] = useState({
     weight:'',
@@ -17,6 +19,7 @@ const [IMC, setIMC] = useState({
 
 // Actualizar Datos
 const actualizarIMC = (evento)=>{
+
     evento.preventDefault();
     setIMC({
         ...IMC,
@@ -28,6 +31,12 @@ const actualizarIMC = (evento)=>{
 const calcularIMC = (evento) => {
     evento.preventDefault();
     let { weight, height } = IMC;
+    if(weight==""||height==""){
+      setError(true)
+      return
+    }
+    setError(false)
+
     calculo = (parseFloat(weight) / (parseInt(height) / 100) ** 2).toFixed(2);
   
     console.log(calculo);
@@ -42,7 +51,16 @@ const calcularIMC = (evento) => {
   };
   
 
+useEffect(()=>{
+  if(error){
+    setTimeout(()=>{
+      setError(false)
+    },2000)
+  }
 
+
+
+},[error])
 
 
 
@@ -53,14 +71,17 @@ const calcularIMC = (evento) => {
 
 <section className="login-box">
     <h2>Formulario IMC</h2>
+    {error?(
+      <article className='Error'>Error Todos los campos son obligatorios</article>
+    ):null}
   <form >
     <div className="user-box">
       <input type="number" name="weight" required="" onChange={actualizarIMC}/>
-      <label>Peso</label>
+      <label className='label_btn'>Peso</label>
     </div>
     <div className="user-box">
       <input type="number" name="height" required="" onChange={actualizarIMC}/>
-      <label>Estatura</label>
+      <label className='label_btn'>Estatura</label>
     </div><center>
     <button className="btn-Form" href="#" onClick={calcularIMC}>
            Calcular
